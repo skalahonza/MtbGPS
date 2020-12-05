e83850294706be8e4972da806407dd6b9dcdf832
Jan Skála <j.skala@hotmail.com>
2020-12-05 10:21:04 +0100

    Dashboard layout.

--- gui.m
+++ gui.m
@@ -1,11 +1,14 @@
 function gui()
 close all force;
-
-%% Grid Layout
 fig = uifigure('Position',[100 100 637 517]);
+
+%% Dashboard
 mainG = uigridlayout(fig,[3 2]);
 mainG.RowHeight = {'8x','2x', 30};
 mainG.ColumnWidth = {'2x' ,'8x'};
+
+%% UI Context Menu
+
 
 %% map
 gx = geoaxes(mainG);
@@ -24,6 +27,16 @@ leftG.RowHeight = {'1x',30,'fit','1x','fit','1x'};
 leftG.Layout.Row = 1;
 leftG.Layout.Column = 1;
 
+%% Distance
+distance = uilabel(leftG);
+distance.Text = '0.00';
+distance.FontSize = 28;
+distance.HorizontalAlignment = 'center';
+distanceL = uilabel(leftG);
+distanceL.Text = 'Distance: KM';
+distanceL.FontSize = 14;
+distanceL.HorizontalAlignment = 'center';
+
 %% Speed
 speed = uigauge(leftG,'semicircular');
 speed.Limits = [0 50];
@@ -32,7 +45,15 @@ speedL.Text = 'Speed: KM/H';
 speedL.HorizontalAlignment = 'center';
 speedL.FontSize = 14;
 
-
+%% Fitness
+fitness = uilabel(leftG);
+fitness.Text = '0';
+fitness.FontSize = 28;
+fitness.HorizontalAlignment = 'center';
+fitnessL = uilabel(leftG);
+fitnessL.Text = 'Energy: kcal';
+fitnessL.FontSize = 14;
+fitnessL.HorizontalAlignment = 'center';
 
 %% elevation
 elevation = uiaxes(mainG, ...
@@ -43,6 +64,7 @@ elevation.Layout.Column = [1, 2];
 elevationL = uilabel(mainG);
 elevationL.FontSize = 14;
 elevationL.HorizontalAlignment = 'center';
+elevationL.Text = 'Elevation: m';
 elevationL.Layout.Column = [1 2];
 %% handlers
 
