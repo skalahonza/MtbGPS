8399d1773f92540fbaeb537529a54cd5776c9f2f
Jan Skála <j.skala@hotmail.com>
2020-12-05 11:28:02 +0100

    Render elevation.

--- gui.m
+++ gui.m
@@ -62,11 +62,39 @@ elevation = uiaxes(mainG, ...
     'YLim', [-100 100]);
 elevation.Layout.Row = 2;
 elevation.Layout.Column = [1, 2];
+elevation.YLim = [0 inf];
+
 elevationL = uilabel(mainG);
 elevationL.FontSize = 14;
 elevationL.HorizontalAlignment = 'center';
 elevationL.Text = 'Elevation: m';
 elevationL.Layout.Column = [1 2];
+
 %% handlers
+[openFileClicked] = uiState();
+
+% Open file clicked
+mOpen.MenuSelectedFcn = @(src, event)openFileClicked(elevation);
+end
+
+function [openFileClicked] = uiState()
+openFileClicked = @openFile;
+
+distance = 0;
+speed = 0;
+calories = 0;
+elevation = [];
+
+function openFile(elevationPlot)
+    [f,p] = uigetfile('*.gpx');
+    if isequal(f,0)
+       disp('User selected Cancel');
+    else
+       route = loadgpx(fullfile(p,f),'ElevationUnits','meters');
+       elevation = route(:,3);       
+       plot(elevationPlot, elevation' ,'-x');
+       elevationPlot.YLim = [min(elevation) max(elevation)];
+    end
+end
 
 end
